34d385b4ec606cc9e18fda3b3e342686eb8f0924
adityagaddhyan <adityagaddhyan@gmail.com>
2020-09-25 00:33:44 +0530

    simple shows list of movie with searched keyword

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+var request = require("request");
+var locus=require("locus");
+app.use(bodyParser.urlencoded([true]));
 
 const APIkey="497020b3";
 
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.send("<h1>Hello World<h1>");
+    res.render("home");
+});
+//function to search data from api and return as an object
+var keyword;
+var resultJSON;
+app.post("/search",async function(req,res){
+    keyword=req.body.searchword;
+    var url="http://www.omdbapi.com/?apikey="+APIkey+"&s="+keyword;
+    // eval(locus);
+    await request(url,function(error,response,body){
+        if(!error && response.statusCode==200){
+            resultJSON=JSON.parse(body);
+        }
+    });
+    res.redirect("/result", )
+});
+app.get("/result",function(req,res){
+    console.log(resultJSON);
+    // eval(locus);
+    res.render("result",{result:resultJSON});
 });
 
 
@@ -12,6 +37,7 @@ app.get("/",function(req,res){
 
 
 
-app.listen(8080,function(){
+
+app.listen(8888,function(){
     console.log("The app is running");
 });
